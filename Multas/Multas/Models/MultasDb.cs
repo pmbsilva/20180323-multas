140fec1f5915d5fa77f8cd2b6a313e0c35d020ac
Pedro Silva <estt13685@ipt.pt>
2018-03-23 17:01:09 +0000

    criação dos modelos da base de dados

---
+++ Multas/Multas/Models/MultasDb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class MultasDb : DbContext
+    {
+        // construtor da classe
+        public MultasDb() : base("MultasDbConnectionString")
+        {
+            // Package Manager Console
+            // PM> Enable-Migrations -EnableAutomaticMigrations
+            // PM> Update-Database -Verbose
+        }
+
+        // identificar as tabelas da base de dados
+        public virtual DbSet<Multas> Multas { get; set; }
+        public virtual DbSet<Condutores> Condutores { get; set; }
+        public virtual DbSet<Viaturas> Viaturas { get; set; }
+        public virtual DbSet<Agentes> Agente { get; set; }
+
+
+    }
+}
---
+++ Multas/Multas/Models/Viaturas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class Viaturas
+    {
+
+        public Viaturas()
+        {
+            ListaDeMultas = new HashSet<Multas>();
+        }
+
+        [Key]
+        public int ID { get; set; }
+        //[Required]
+        public string Matricula { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+
+        // dados do dono da viatura
+        public string NomeDono { get; set; }
+        public string MoradaDono { get; set; }
+        public string CodPostalDono { get; set; }
+
+        // complementar a informação sobre o relacionamento
+        // de uma Viatura com as Multas a ela relacionadas
+        public virtual ICollection<Multas> ListaDeMultas { get; set; }
+    }
+}
